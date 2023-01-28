c31950e7c099e16ab28e1ccdc068231e07679b2c
LITZZ <76871632+Loganlitzz@users.noreply.github.com>
2023-01-29 00:17:27 +0530

    Add files via upload

---
+++ themepref.js
@@ -0,0 +1,6 @@
+var i=document.getElementById('toggle').value;
+var element=document.getElementByQuery('body');
+if(i==on)
+{
+    element.classList.toggle("dark-theme");
+}
---
+++ validate.js
@@ -0,0 +1,14 @@
+console.log("Js working");
+document.getElementById('result').innerHTML="Hello";
+// function fun1(){
+// var b1,b2,b3,b4,b5,b6,b7,b8,b9;
+// b1=document.getElementById("b1");
+// b2=document.getElementById("b2");
+// b3=document.getElementById("b3");
+// b4=document.getElementById("b4");
+// b5=document.getElementById("b5");
+// b6=document.getElementById("b6");
+// b7=document.getElementById("b7");
+// b8=document.getElementById("b8");
+// b9=document.getElementById("b9");
+// }
